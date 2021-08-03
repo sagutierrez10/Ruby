6cb445bc4b8ded58bbe3fe65051feda5f69f1ace
Susana Gutierrez <susanag995@gmail.com>
2021-08-02 23:32:57 -0600

    range

---
+++ guessTheNumber/range.rb
@@ -0,0 +1,9 @@
+# .include?(value) => true or false
+puts ("a".."z").include?("g")
+puts ("a".."z").include?("A") 
+# .last => returns the last object in range
+puts ("a".."z").last
+# .max => returns the maximum value in range
+puts (1..5).max
+# .min => returns the minimum value in range
+puts (1..5).min
